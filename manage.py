4c9f5713389844222baaf1464692d0c3375b36f8
jluarter <jluarter@163.com>
2018-03-07 20:02:37 +0800

    使用 Flask Script

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    # 确保有导入 Flask app object，否则启动的 CLI 上下文中仍然没有 app 对象
+    # 确保有导入 Flask app object，否则启动的 CLI 上下文中仍然没有 app对象
     return dict(app=main.app)
 
 if __name__=='__main__':
