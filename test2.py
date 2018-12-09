4dc715cf4eba9d2250724278792e594d39fd1b61
jluarter <jluarter@163.com>
2018-12-09 18:43:13 +0800

    Usage of GIT

---
+++ test2.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+git init
+git remote add origin git@github.com:username/project-name.github
+git fetch origin remote-branch-name
+git checkout -b local-branch-name origin/remote-branch-name
+
+
+edit
+
+
+git add .
+git commit -m "log"
